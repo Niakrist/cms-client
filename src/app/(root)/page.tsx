a0eb934f48afee1df2e33a4687cae5c5d7514160
Niakrist <ak_88dron@inbox.ru>
2025-11-01 11:29:03 +0300

    init project

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next'
+import { Home } from './Home'
+
+export const metadata: Metadata = {
+  title: 'Интернент магазин',
+  description: 'Интернент магазин - описание'
+}
+
+export default function HomePage() {
+  return (
+    <div>
+      some text
+      <Home />
+    </div>
+  )
+}
---
+++ src/providers/Providers.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { useState, type PropsWithChildren } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
+
+export function Providers({ children }: PropsWithChildren) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false
+        }
+      }
+    })
+  )
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <Toaster />
+    </QueryClientProvider>
+  )
+}
