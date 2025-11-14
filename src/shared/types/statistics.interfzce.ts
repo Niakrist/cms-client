c37d80320a3cafcecf1eb85bcf64e79fe5ccaa0e
Niakrist <ak_88dron@inbox.ru>
2025-11-14 11:10:37 +0300

    feat: emplement interface

---
+++ src/shared/types/statistics.interfzce.ts
@@ -0,0 +1,24 @@
+export interface IMainStatistics {
+  id: number
+  name: string
+  value: number
+}
+
+export interface IMonthlySales {
+  date: string
+  value: number
+}
+
+// Последние 5 пользователей оплативших в нашем магазине
+export interface ILastUsers {
+  id: string
+  name: string
+  email: string
+  picture: string
+  total: number
+}
+
+export interface IMiddleStatistics {
+  monthlySales: IMainStatistics[]
+  lastUsers: ILastUsers[]
+}
--- src/shared/types/user.interface.ts
+++ src/shared/types/user.interface.ts
@@ -1,4 +1,6 @@
-import { IOrder, IProduct, IStore } from './store.interface'
+import { IOrder } from './order.interface'
+import { IProduct } from './product.interface'
+import { IStore } from './store.interface'
 
 export interface IUser {
   id: string
