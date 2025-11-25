2f9a5483cf821f7da61d4d47f95150a52a2abba5
Niakrist <ak_88dron@inbox.ru>
2025-11-25 16:54:12 +0300

    feat: emplement product table

---
+++ src/components/ui/data-loading/DataTableLoading.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from '@/components/ui/Skeleton'
+import { Card, CardContent } from '../Card'
+import { Loader } from '../Loader'
+
+export function DataTableLoading() {
+  return (
+    <div className='max-w-screen-2xl mx-auto w-full'>
+      <Skeleton className='h-8 w-48' />
+      <Skeleton className='h-8 w-72 mt-6' />
+      <Card className='mt-6'>
+        <CardContent>
+          <div className='h-[520px] w-full flex items-center justify-center'>
+            <Loader />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
---
+++ src/hooks/queries/products/useDeleteProducts.ts
@@ -0,0 +1,35 @@
+'use client'
+
+import { PUBLIC_URL, STORE_URL } from '@/config/url.config'
+import { productService } from '@/services/product.service'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useParams, useRouter } from 'next/navigation'
+import { useMemo } from 'react'
+import toast from 'react-hot-toast'
+
+export const useDeleteProducts = () => {
+  const params = useParams<{ productId: string; storeId: string }>()
+  const router = useRouter()
+  const queryClient = useQueryClient()
+
+  const { mutate: deleteProduct, isPending: IsLoadingDeleteProduct } =
+    useMutation({
+      mutationKey: ['delete product'],
+      mutationFn: () => productService.delete(params.productId),
+      onSuccess() {
+        queryClient.invalidateQueries({
+          queryKey: ['get products for store dashboard']
+        })
+        toast.success('Товар успешно удален')
+        router.push(STORE_URL.products(params.storeId))
+      },
+      onError() {
+        toast.error('Не удалось удалить товар')
+      }
+    })
+
+  return useMemo(
+    () => ({ deleteProduct, IsLoadingDeleteProduct }),
+    [deleteProduct, IsLoadingDeleteProduct]
+  )
+}
