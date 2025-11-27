cbd89c959b4096a8d31995c6112b08a1197aaf19
Niakrist <ak_88dron@inbox.ru>
2025-11-27 16:40:50 +0300

    feat: eplement upload photos and ProductEditPage

---
+++ src/app/store/[storeId]/products/[productId]/page.tsx
@@ -0,0 +1,13 @@
+import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+import { Metadata } from 'next'
+import React from 'react'
+import { ProductEdit } from './ProductEdit'
+
+export const metadata: Metadata = {
+  title: 'Настройки товара',
+  ...NO_INDEX_PAGE
+}
+
+export default function ProductEditPage() {
+  return <ProductEdit />
+}
---
+++ src/hooks/queries/categories/useUpdateCategory.ts
@@ -0,0 +1,36 @@
+'use client'
+import { STORE_URL } from '@/config/url.config'
+import { categoryService } from '@/services/category.service'
+import { ICategoryInput } from '@/shared/types/category.interface'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useParams, useRouter } from 'next/navigation'
+import { useMemo } from 'react'
+import toast from 'react-hot-toast'
+
+export function useUpdateCategory() {
+  const params = useParams<{ categoryId: string }>()
+  const queryClient = useQueryClient()
+  const { push } = useRouter()
+
+  const { mutate: updateCategory, isPending: isLoadingUpdateCategory } =
+    useMutation({
+      mutationKey: ['update category'],
+      mutationFn: (data: ICategoryInput) => {
+        return categoryService.update(data, params.categoryId)
+      },
+      onSuccess() {
+        queryClient.invalidateQueries({
+          queryKey: ['get catgories for store dashboard']
+        })
+        toast.success('Категория успешно обновлена')
+        push(STORE_URL.categories(params.categoryId))
+      },
+      onError() {
+        toast.error('Ошибка при обнавлении категории')
+      }
+    })
+  return useMemo(
+    () => ({ updateCategory, isLoadingUpdateCategory }),
+    [updateCategory, isLoadingUpdateCategory]
+  )
+}
